62037c03ac956ea54ccc1a05c52864824a1f40ec
jeff vax <jefffvasss@gmail.com>
2026-05-07 02:59:41 +0100

    Update site-config.js

--- site-config.js
+++ site-config.js
@@ -1,13 +1,13 @@
 module.exports = {
-  siteName: "9ja News",
-  baseUrl: "https://blog.pluse.name.ng",
+  siteName: "9ja News pluse",
+  baseUrl: "https://pluse.name.ng",
   description: "Latest news, politics, sports and trending stories from Nigeria and beyond.",
 
   language: "en",
 
   author: {
     name: "Ifeanyi Clement Okoye",
-    url: "https://blog.pluse.name.ng/authors/ifeanyi-okoye",
+    url: "https://pluse.name.ng/authors/ifeanyi-okoye.html", 
     jobTitle: "Editor"
   }
 };
